a371c19e4921c02350272fc893c034a7fe3f4346
k-pkp <kengpakkapol2005@gmail.com>
2025-04-10 18:37:13 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,36 +1,35 @@
 public class SortMyNumbersPlease {
 
     public static void doTheSortingThing(int[] theNumbers) {
-  int n = theNumbers.length;
-       boolean didSwap = true;
-       while (didSwap) {
-   didSwap = false;
-   for (int i = 0; i < n - 1; i++) {
- if (theNumbers[i] > theNumbers[i + 1]) {
-           int temp = theNumbers[i+1];
-           theNumbers[i + 1] = theNumbers[i];
- theNumbers[i] = temp;
- didSwap = true;
-           }
-       }
-       n--; // Oops! Decrementing n too early
-   }
+        int theLength = theNumbers.length;
+        boolean somethingWasSwapped = true;
+        while (somethingWasSwapped) {
+            somethingWasSwapped = false;
+            for (int j = 0; j < theLength - 1; j++) {
+                if (theNumbers[j] > theNumbers[j + 1]) {
+                    int temporaryValue = theNumbers[j + 1];
+                    theNumbers[j + 1] = theNumbers[j];
+                    theNumbers[j] = temporaryValue;
+                    somethingWasSwapped = true;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
- int[] someRandomNumbers = {5, 1, 4, 2, 8};
- System.out.println("Before sorting:");
- for (int num : someRandomNumbers) {
- System.out.print(num + " ");
- }
- System.out.println();
-
- SortMyNumbersPlease.doTheSortingThing(someRandomNumbers);
-
- System.out.println("After sorting (maybe):");
- for (int val : someRandomNumbers) {
- System.out.print(val + " ");
+        int[] listOfNumbers = {5, 1, 4, 2, 8};
+        System.out.println("Before sorting:");
+        for (int number : listOfNumbers) {
+            System.out.print(number + " ");
         }
- System.out.println();
+        System.out.println();
+
+        SortMyNumbersPlease.doTheSortingThing(listOfNumbers);
+
+        System.out.println("After sorting:");
+        for (int value : listOfNumbers) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
     }
 }
